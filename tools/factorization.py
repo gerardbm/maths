c32ec4d40dc2e9fb5e70f8f09e7a70822cd9b0b7
Gerard <gerardbajona@gmail.com>
2026-02-21 13:53:29 +0100

    Update factorization.py

--- tools/factorization.py
+++ tools/factorization.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # --------------------------------------------------
-# Name    : Prime Factors Decomposition
-# Version : 1.0.2
+# Name    : Factorization in prime factors
+# Version : 2.0.0
 # Python  : 3.13.5
 # License : MIT
 # Author  : Gerard Bajona
@@ -12,44 +12,19 @@
 # --------------------------------------------------
 """Tool to factorize a number into prime factors."""
 
-import time
+import argparse
 
-def main_menu():
-    """Show the main options menu and prompt the user to pick one."""
-    print()
-    print("Prime Factors Decomposition")
-    print("---------------------------")
-    print("Options menu:")
-    print("  1. Enter a number")
-    print("  0. Quit")
-    return get_option(0, 1)
-
-def get_option(first, last):
-    """Get and validate the option value as integer."""
-    while True:
-        try:
-            print()
-            option = int(input("Enter an option: "))
-        except ValueError:
-            print("> It must be an integer number.")
-            continue
-        else:
-            if option < first or option > last:
-                print("> Unknown option. Try it again.")
-                continue
-        return option
-
-def get_number():
-    """Get a number to decompose into prime factors."""
-    while True:
-        try:
-            print()
-            number = int(input("Enter a number to decompose: "))
-            break
-        except ValueError:
-            print("> Wrong format, type numbers instead.")
-            print("> (Decimals not allowed: type an integer).")
-    return number
+def parse_arguments():
+    """Parse command-line arguments."""
+    parser = argparse.ArgumentParser(
+            description="Factorization tool for the command line.")
+    parser.add_argument('number',
+            type=int,
+            help="Integer value to factorize in prime factors")
+    args = parser.parse_args()
+    if args.number < 2:
+        parser.error("The number must be an integer greater than 1.")
+    return args.number
 
 def decomposition(number):
     """Decompose the number into prime factors!"""
@@ -99,33 +74,19 @@ def output(joined, columns, number, factors, divisor):
     print()
     print(" " + ("-" * (lennum)) + ("-" * lendiv))
     print(columns)
-    print('Prime decomposition:')
-    print('>>', number, '=', ' * '.join(map(str, factors)))
+    print('Factorization in prime factors:')
+    print('>', number, '=', ' * '.join(map(str, factors)))
     print()
     print('Exponential form:')
-    print('>>', number, '=', joined)
-    print()
+    print('>', number, '=', joined)
 
 def main():
     """Main program."""
-    main_option = 1
-    while main_option != 0:
-        main_option = main_menu()
-        if main_option == 1:
-            number = get_number()
-            start = time.time()
-            columns, factors, divisor = decomposition(number)
-            grouped = format_group(factors)
-            joined = format_join(grouped)
-            output(joined, columns, number, factors, divisor)
-            end = time.time()
-            print('Time required:')
-            print('>>', round(end-start, 5), 'sec')
-            print()
-            input("Press Enter to continue...")
-        else:
-            print()
-            print("Good bye :-)")
+    number = parse_arguments()
+    columns, factors, divisor = decomposition(number)
+    grouped = format_group(factors)
+    joined = format_join(grouped)
+    output(joined, columns, number, factors, divisor)
 
 if __name__ == '__main__':
     main()
